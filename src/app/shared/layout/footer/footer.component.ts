93d3aa9a2ba6509117f6a37e8cf22603c9fbfd02
Kavita Gupta <kavitagupta.gupta123@gmail.com>
2023-06-12 00:36:45 +0530

    header complete

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -3,7 +3,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-layout-footer',
   templateUrl: './footer.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
   today: number = Date.now();
