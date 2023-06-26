7ab9083b2da8d433ecf380601909b9e69265a01d
Kavita Gupta <kavitagupta.gupta123@gmail.com>
2023-06-26 22:40:29 +0530

    activity wip

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -12,7 +12,8 @@ export class LocationComponent implements OnInit {
   displayedColumns: string[] = ['key', 'value'];
   locationId: string;
   locationDetails!: ILocation;
-  dataSource!: any;
+  activityDatasource!: any;
+  overviewDatasource!: any;
   constructor(private route: ActivatedRoute, private homeSvc: HomeService) {
     this.locationId = this.route.snapshot.params['locationId'] ?? '';
   }
@@ -21,18 +22,30 @@ export class LocationComponent implements OnInit {
       next: (data) => {
         this.locationDetails = data;
         this.overviewTab();
+        this.activityTab();
       },
       error: (error) => {},
     });
   }
 
   overviewTab() {
-    console.log(this.locationDetails);
-    this.dataSource = [
+    this.overviewDatasource = [
       { key: 'Description', value: this.locationDetails.description },
       { key: 'Country', value: this.locationDetails.country },
       { key: 'State', value: this.locationDetails.state },
       { key: 'City', value: this.locationDetails.city },
+    ];
+  }
+  activityTab() {
+    this.activityDatasource = [
+      {
+        key: 'Activities',
+        value: this.locationDetails.activities
+          .map((ele) => {
+            return ele.type;
+          })
+          .join(', '),
+      },
     ];
   }
 }
